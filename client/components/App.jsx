d5d642d151b21989d66326033363a133ec62c935
SharleneRyan <sa.ryan@live.com>
2019-05-16 09:02:39 +1200

    update

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,49 +1,47 @@
-import React, { Component } from 'react';
-import { HashRouter as Router, Route, Link }  from 'react-router-dom'
-import { connect } from 'react-redux'
-import * as actions from '../actions'
+import React, { Component } from "react";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 //MATERIAL UI IMPORTS
 
-import Header from './Header'
-import ParentRegister from './ParentRegister'
-import ParentLogin from './ParentLogin'
-import ParentDashboard from './ParentDashboard'
-import ChildRegister from './ChildRegister'
-import EccList from './EccList'
-import FilterLocation from './FilterLocation'
-import WaitlistApplication from './WaitlistApplication'
-import LandingPage from './LandingPage'
+import Header from "./Header";
+import ParentRegister from "./ParentRegister";
+import ParentLogin from "./ParentLogin";
+import ParentDashboard from "./ParentDashboard";
+import ChildRegister from "./ChildRegister";
+import EccList from "./EccList";
+import FilterLocation from "./FilterLocation";
+import WaitlistApplication from "./WaitlistApplication";
+import LandingPage from "./LandingPage";
 
-import EccRegister from './EccRegister'
-import EccLogin from './EccLogin'
-import EccDashboard from './EccDashboard'
-import EccSettings from './EccSettings'
+import EccRegister from "./EccRegister";
+import EccLogin from "./EccLogin";
+import EccDashboard from "./EccDashboard";
+import EccSettings from "./EccSettings";
 
 class App extends React.Component {
   render() {
-    
     return (
-    <Router>
-      <div>
-        <Route path exact="/" component={LandingPage}/>
-        <Route path="/parent/register" component={ParentRegister}/>
-        <Route path="/parent/login" component={ParentLogin}/>
-        <Route path="/parent/home" component={ParentDashboard}/>
-        <Route path="/parent/registerchild" component={ChildRegister}/>
-        <Route path="/parent/filter" component={FilterLocation}/>
-        <Route path="/parent/ecclist" component={EccList}/>
-        <Route path="/parent/registerwaitlist" component={WaitlistApplication}/>
-        <Route path="/ecc/home" component={EccDashboard}/>
-        <Route path="/ecc/register" component={EccRegister}/>
-        <Route path="/ecc/login" component={EccLogin}/>
-        <Route path="/ecc/settings" component={EccSettings}/>
-      </div>
-    </Router>
+      <Router>
+        <div>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/parent/register" component={ParentRegister} />
+          <Route path="/parent/login" component={ParentLogin} />
+          <Route path="/parent/home" component={ParentDashboard} />
+          <Route path="/parent/registerchild" component={ChildRegister} />
+          <Route path="/parent/filter" component={FilterLocation} />
+          <Route path="/parent/ecclist" component={EccList} />
+          <Route path="/parent/registerwaitlist"
+            component={WaitlistApplication} />
+          <Route path="/ecc/home" component={EccDashboard} />
+          <Route path="/ecc/register" component={EccRegister} />
+          <Route path="/ecc/login" component={EccLogin} />
+          <Route path="/ecc/settings" component={EccSettings} />
+        </div>
+      </Router>
     );
-
- 
   }
-}   
-      
-export default App
+}
+
+export default App;
